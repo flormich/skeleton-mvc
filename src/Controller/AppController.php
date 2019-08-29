45651ccc180fc421b45e7b38f83598c6eba1b1a4
Florian Michel <43203255+flormich@users.noreply.github.com>
2019-08-29 10:54:35 +0200

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -9,6 +9,8 @@ namespace App\Controller;
             return $this->render("app/index.html.php", [
                 "message" => "Hello world !!",
                 "suite" => "Encore un nouveau message"
+                "suite2" => "Hello page !!",
+                "explication" => 'Aller dans template/app/index.html.php et mettre "$message" afin d\'affiche ce qu\'il y a dans "message" de src/Controller/AppController.php'
             ]);
         }
     }
